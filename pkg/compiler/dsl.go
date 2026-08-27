8e66733e58c93eb37f312161f747eab4bc8e9656
AboEl3iz <k.hassan202077@gmail.com>
2026-08-27 11:49:24 +0300

    complete DSL for parsing config yml

--- pkg/compiler/dsl.go
+++ pkg/compiler/dsl.go
@@ -2,9 +2,9 @@ package compiler
 
 // Policy represents the top-level YAML policy structure.
 type Policy struct {
-	APIVersion string `yaml:"apiVersion"`
-	Kind       string `yaml:"kind"`
-	Metadata   Metadata `yaml:"metadata"`
+	APIVersion string     `yaml:"apiVersion"`
+	Kind       string     `yaml:"kind"`
+	Metadata   Metadata   `yaml:"metadata"`
 	Spec       PolicySpec `yaml:"spec"`
 }
 
@@ -18,11 +18,40 @@ type PolicySpec struct {
 }
 
 type Rule struct {
-	ID          uint32   `yaml:"id"`
-	Name        string   `yaml:"name"`
-	Action      string   `yaml:"action"` // "allow" or "deny"
-	SrcCIDRs    []string `yaml:"srcCIDRs,omitempty"`
-	DstPorts    []uint16 `yaml:"dstPorts,omitempty"`
-	Protocols   []string `yaml:"protocols,omitempty"`
-	SrcCgroups  []string `yaml:"srcCgroups,omitempty"`
+	ID         uint32   `yaml:"id"`
+	Name       string   `yaml:"name"`
+	Action     string   `yaml:"action"` // "allow" or "deny"
+	SrcCIDRs   []string `yaml:"srcCIDRs,omitempty"`
+	DstPorts   []uint16 `yaml:"dstPorts,omitempty"`
+	Protocols  []string `yaml:"protocols,omitempty"`
+	SrcCgroups []string `yaml:"srcCgroups,omitempty"`
 }
+
+// CompiledPolicy represents verified and expanded policy elements ready for BPF staging.
+type CompiledPolicy struct {
+	Name          string
+	DefaultAction string
+	CIDRBlocks    []CompiledCIDR
+	CgroupBlocks  []CompiledCgroup
+	PortRules     []CompiledPortRule
+}
+
+type CompiledCIDR struct {
+	RuleID  uint32
+	CIDRStr string
+	Action  string
+}
+
+type CompiledCgroup struct {
+	RuleID     uint32
+	CgroupPath string
+	Action     string
+}
+
+type CompiledPortRule struct {
+	RuleID   uint32
+	DstPort  uint16
+	Protocol string // "tcp", "udp", "icmp"
+	Action   string // "allow", "deny"
+}
+
